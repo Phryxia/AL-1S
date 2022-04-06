e431d847d9531dcc038ed8db15c8285540654e3c
gfnuko <xahhaepica@gmail.com>
2022-04-06 23:37:56 +0900

    Implement birth assign

---
+++ src/arisServices/birth/assign/context.ts
@@ -0,0 +1,85 @@
+import { Temporal } from '@js-temporal/polyfill'
+import type { ArisContext, ArisContextState } from '@src/types/context'
+import { parseDate } from '@src/utilities/date'
+
+const enum BirthAssignStateName {
+  ASK_NAME,
+  ASK_DATE,
+}
+
+export interface BirthAssignStore {
+  name: string
+  date: Temporal.PlainDate
+}
+
+const INITIAL_STORE: BirthAssignStore = {
+  name: '',
+  date: Temporal.Now.plainDateISO(),
+}
+
+export function createBirthAssignContext(
+  store: BirthAssignStore = { ...INITIAL_STORE },
+): ArisContext<BirthAssignStore> {
+  return {
+    contextName: 'birth-assign-context',
+    store,
+    states: [
+      {
+        stateName: 'ask-name',
+        transitions: [
+          {
+            toId: BirthAssignStateName.ASK_DATE,
+            shouldBeTriggered: (message) => {
+              return (message.text?.trim().length ?? 0) >= 2
+            },
+            onTransition: (message) => {
+              store.name = message.text?.trim() ?? ''
+
+              return {
+                text: `${message.text}의 생일은 언제입니까?`,
+              }
+            },
+          },
+        ],
+        defaultTransition: {
+          toId: BirthAssignStateName.ASK_NAME,
+          onTransition: (message) => {
+            return {
+              text: `${message.text}는 이름같지 않습니다. 2글자 이상으로 입력해주세요.`,
+            }
+          },
+        },
+      },
+      {
+        stateName: 'ask-date',
+        transitions: [
+          {
+            toId: -1,
+            shouldBeTriggered: (message) => {
+              try {
+                parseDate(message.text ?? '')
+                return true
+              } catch {
+                return false
+              }
+            },
+            onTransition: (message) => {
+              store.date = parseDate(message.text ?? '')
+              return {
+                text: `${store.name}의 생일은 ${store.date.toString()}입니다.`,
+              }
+            },
+          },
+        ],
+        defaultTransition: {
+          toId: BirthAssignStateName.ASK_DATE,
+          onTransition: (message) => {
+            return {
+              text: '날짜를 잘 이해하지 못했어요. 다음과 같이 입력해보세요\n1994-05-07',
+            }
+          },
+        },
+      },
+    ],
+  }
+}
--- src/arisServices/birth/assign/index.ts
+++ src/arisServices/birth/assign/index.ts
@@ -1,33 +1,23 @@
 import type { TextRouterConfig, TextRouterContext } from '@src/apps/routeMessage'
-import * as FriendDAO from '@src/service/dao/friend'
+import { addContext } from '@src/apps/runArisService'
+// import * as FriendDAO from '@src/service/dao/friend'
+import { createBirthAssignContext } from './context'
 
-async function assignBirth(name: string, birthday: Date): Promise<void> {
-  FriendDAO.addFriend({
-    name,
-    birthday,
-    waifus: [],
-  })
-}
+// async function assignBirth(name: string, birthday: Date): Promise<void> {
+//   FriendDAO.addFriend({
+//     name,
+//     birthday,
+//     waifus: [],
+//   })
+// }
 
 const config: TextRouterConfig = {
   rules: [],
-  onMatchedCompletly: async ({ currentToken }: TextRouterContext) => {
-    if (!currentToken) {
-      return {
-        text: '아리스는 아무것도 듣지 못했습니다.',
-      }
-    }
+  onMatchedCompletly: async ({ message }: TextRouterContext) => {
+    addContext(message.chat.id, message.from?.id ?? 0, createBirthAssignContext())
 
-    try {
-      await assignBirth(currentToken, new Date())
-
-      return {
-        text: `아리스는 이해했습니다.\n당신의 이름은 ${currentToken}입니다.`,
-      }
-    } catch (error) {
-      return {
-        text: `오류 발생! 아리스의 머리가 어지럽습니다.\n${error}`,
-      }
+    return {
+      text: '선생님은 누구의 생일을 챙기고 싶습니까?',
     }
   },
 }
