a3130b685407d6fd4e67e0cd4401aec8e211c8f7
gfnuko <xahhaepica@gmail.com>
2022-05-04 22:39:54 +0900

    Implement birth list using ascii art

--- src/arisServices/birth/list/index.ts
+++ src/arisServices/birth/list/index.ts
@@ -1,10 +1,10 @@
 import type { TextRouterConfig, TextRouterContext } from '@src/apps/routeMessage'
-import { getBirthlistByChatId } from './logic'
+import { getBirthListMessageByChatId } from './logic'
 
 const config: TextRouterConfig = {
   rules: [],
   onMatchedCompletly: async ({ message }: TextRouterContext) => {
-    const birthList = await getBirthlistByChatId(message.chat.id)
+    const birthList = await getBirthListMessageByChatId(message.chat.id)
     return {
       text: birthList,
     }
--- src/arisServices/birth/list/logic.ts
+++ src/arisServices/birth/list/logic.ts
@@ -7,16 +7,14 @@ type Birth = {
   waifus: any[]
 }
 
-type RefinedBirth = {
-  name: string
-  birthday: string
+type RefinedBirth = Birth & {
   remainDays: number
 }
 
 // 생일까지 남은 날짜를 계산하는 함수
-const getRemainDays = (baseDay: Temporal.PlainDate, birthday: Temporal.PlainDate) => {
-  const currentYear = Temporal.Now.PlainDate('iso8601').year
-  baseDay = baseDay.with({ year: currentYear  })
+function getRemainDays(baseDay: Temporal.PlainDate, birthday: Temporal.PlainDate) {
+  const currentYear = Temporal.Now.plainDate('iso8601').year
+  baseDay = baseDay.with({ year: currentYear })
   birthday = birthday.with({ year: currentYear })
 
   const duration = baseDay.until(birthday)
@@ -56,26 +54,45 @@ const births: Birth[] = [
   },
 ]
 
-export const getBirthlistByChatId = async (chatId: number) => {
+export async function getBirthListMessageByChatId(chatId: number): Promise<string> {
   // db에서 찾는걸 모의함
-  const birthsOfCurrentChat = births
+  const refinedBirthList: RefinedBirth[] = births
     .filter((birth) => birth.chatId === chatId)
     .map((birth) => {
       const remainDays = getRemainDays(Temporal.Now.plainDateISO(), birth.birthday)
       return {
-        name: birth.name,
-        birthday: birth.birthday.toString(),
+        ...birth,
         remainDays,
       }
     })
     .sort((a, b) => a.remainDays - b.remainDays)
-    .map((birth) => {
-      return `${birth.name}(${birth.birthday}) : ${birth.remainDays}일 남았습니다.`
-    })
-  const prefix = '생일자 리스트입니다.'
-  const postfix = '미리미리 축하해주세요'
-  return decodeURIComponent(`${prefix}
-${birthsOfCurrentChat.join('\n')}
-${postfix}
-`)
+
+  return renderBirthList(refinedBirthList)
+}
+
+function renderBirthList(birthList: RefinedBirth[]): string {
+  return `생일자 리스트입니다. 미리미리 축하해주세요.
+${birthList
+  .map(({ name, birthday, remainDays }) => {
+    const rate = 1 - remainDays / 365
+    return `${name}(${birthday.month}월 ${birthday.day}일) - D-${remainDays}
+${renderProgressBar(rate, 10)} (${formatNumber(rate * 100, 2)}%)`
+  })
+  .join('\n\n')}`
+}
+
+function formatNumber(x: number, digit: number): string {
+  const base = 10 ** digit
+  const roundedXString = `${Math.round(x * base) / base}`
+  const positionOfDot = roundedXString.lastIndexOf('.')
+
+  if (positionOfDot === -1) return roundedXString
+
+  return roundedXString.slice(0, positionOfDot + 3)
+}
+
+function renderProgressBar(rate: number, size: number): string {
+  const numOfBlacks = Math.round(size * rate)
+  const numOfWhites = size - numOfBlacks
+  return `${'■'.repeat(numOfBlacks)}${'□'.repeat(numOfWhites)}`
 }
